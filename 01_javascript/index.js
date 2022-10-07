3d54848846abbf707f5a95bddde3e626a587c87f
Adrian <adrian.talonia11@gmail.com>
2022-10-06 23:23:34 -0500

    completed refresh javascript concepts

--- 01_javascript/index.js
+++ 01_javascript/index.js
@@ -35,4 +35,25 @@ const newObject = { ...oldObject, newProp: 5 };
 
 const filter = (...args) => {
   return args.filter((el = el == 1));
-}
+};
+
+// destructuring
+const numbers = [1, 2, 3];
+[num1, , num2] = numbers;
+console.log(num1, num2);
+
+// Reference
+const person1 = {
+  name: "test",
+};
+
+const secondPerson = person1;
+person.name = "test2";
+console.log(secondPerson.name); // "test2"
+
+const thirdPerson = { ...person1 };
+
+// Refreshing Array Functions
+const numb = [1, 2, 3];
+
+const doubleArray = numb.map((n) => n * 2);
